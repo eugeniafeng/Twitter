34c94c9fc17f52683bf061f8d99e88667e864fe7
eugeniafeng <eugeniafeng@gmail.com>
2021-06-28 19:01:06 -0400

    Implemented functionality to allow user to log out

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -64,5 +65,10 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure!" + response, throwable);
             }
         });
+    }
+
+    public void onLogoutButton(View view) {
+        client.clearAccessToken(); // forget who's logged in
+        finish(); // navigate back to login screen
     }
 }
